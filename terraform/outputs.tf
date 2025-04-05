ddb5d11168a959cff6cfd01bcedf68521590fb76
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2025-04-05 19:17:16 +0200

    Add outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,15 @@ output "kubeconfig" {
   value     = module.kube-hetzner.kubeconfig
   sensitive = true
 }
+
+output "cert_manager_values" {
+  value = module.kube-hetzner.cert_manager_values
+}
+
+output "traefik_values" {
+  value = module.kube-hetzner.traefik_values
+}
+
+output "cilium_values" {
+  value = module.kube-hetzner.cilium_values
+}
